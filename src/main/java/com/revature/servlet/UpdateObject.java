757a97ccf1f468679597646f7a82906bc8da9ad1
Isaiah <isaiahsanason@gmail.com>
2021-11-21 15:41:53 -0800

    Finished CreateObject Servlet, started UpdateObject

--- src/main/java/com/revature/servlet/UpdateObject.java
+++ src/main/java/com/revature/servlet/UpdateObject.java
@@ -1,25 +1,20 @@
 package com.revature.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.models.Dog;
+import com.revature.models.User;
+import com.revature.services.ORM;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.revature.models.*;
-import com.revature.services.ORM;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-
-@WebServlet(urlPatterns = "/create")
-public class CreateObject extends HttpServlet {
+public class UpdateObject extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String objectType = req.getHeader("Object_Type");
@@ -31,30 +26,22 @@ public class CreateObject extends HttpServlet {
         classTypes.add(User.class);
         Class<?> objectClassType = null;
         ObjectMapper mapper = new ObjectMapper();
-        boolean recordAdded = false;
+        boolean recordUpdated = false;
         Object obj = null;
 
         for (Class<?> clazz : classTypes) {
             if (req.getHeader("Object_Type").equals(clazz.getSimpleName()))
                 objectClassType = clazz;
         }
+
         if (contentType.equals("application/json")) {
             try {
                 obj = mapper.readValue(collectorBody, objectClassType);
-                if (ORM.addRecord(obj))
-                    recordAdded = true;
+                if (ORM.updateRecord(obj))
+                    recordUpdated = true;
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        }
-        System.out.println();
-        if (recordAdded) {
-            resp.setStatus(201);
-            resp.setHeader("Content-Type", "application/json");
-            resp.getWriter().println(mapper.writeValueAsString(obj));
-        } else {
-            resp.setStatus(200);
-            resp.getOutputStream().println("<h2>Invalid Object to add</h2>");
         }
 
     }
